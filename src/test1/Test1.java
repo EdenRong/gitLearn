9b248adfbcea3e49cba3a5fbf7b6fb8b0ee837b4
edenrong <749988695@qq.com>
2019-06-09 22:17:12 +0800

    对对对

--- src/test1/Test1.java
+++ src/test1/Test1.java
@@ -8,9 +8,7 @@ public class Test1 {
 
 		System.out.println("328833331123");
 
-		System.out.println("32773331123");
-
-		System.out.println("123");
+		
 
 		System.out.println("123");
 		System.out.println("321");
